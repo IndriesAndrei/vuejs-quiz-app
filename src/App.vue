6f14bdbdcf85bb3f8dd13a1102fd7240e604f3ba
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-07-29 21:16:44 +0300

    Separate logic into different components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
  import q from "./data/quizes.json";
  import { ref, watch } from "vue";
+ import Card from './components/Card.vue';
 
  const quizes = ref(q);
  const search = ref("");
 
- // what we want to watch for changes and what should happen if that changes
+ // watch() -> what we want to watch for changes and what should happen if that changes
  watch(search, () => {
   quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
  })
@@ -22,17 +23,12 @@
       />
     </header>
     <div class="options-container">
-      <div 
+      <!-- we send the props quiz to the Card component -->
+      <Card 
         v-for="quiz in quizes" 
-        :key="quiz.id" 
-        class="card"
-      >
-        <img :src="quiz.img" alt="">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }}</p>
-        </div>
-      </div>
+        :key="quiz.id"
+        :quiz="quiz"
+      />
     </div>
   </div>
 </template>
@@ -66,30 +62,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-
-/* Cards styles */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 199px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 </style>
