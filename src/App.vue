ed0f2d6ff09460156027d345c295194cee282ebe
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-07-29 20:33:23 +0300

    Working Search input

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,95 @@
-z<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+ import q from "./data/quizes.json";
+ import { ref, watch } from "vue";
+
+ const quizes = ref(q);
+ const search = ref("");
+
+ // what we want to watch for changes and what should happen if that changes
+ watch(search, () => {
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+ })
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input 
+        v-model.trim="search" 
+        type="text" 
+        placeholder="Search..." 
+      />
+    </header>
+    <div class="options-container">
+      <div 
+        v-for="quiz in quizes" 
+        :key="quiz.id" 
+        class="card"
+      >
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }}</p>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
 header {
-  line-height: 1.5;
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+header input {
+  border: none;
+  background-color: lightblue;
+  padding: 10px;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* Cards styles */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 199px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
 }
 </style>
