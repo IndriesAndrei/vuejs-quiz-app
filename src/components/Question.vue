c4a71103bf22be442bd6432f61bbea9c939af956
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-07-31 16:52:23 +0300

    Complete the Quiz App

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { defineEmits } from "vue";
 const {question} = defineProps(['question']);
+
+// to get data from child to parent (child-parent communication) we need to emit an event (we emit to QuizView parent component)
+const emit = defineEmits(['selectOption']);
+
+const emitSelectedOption = (isCorrect) => {
+    emit("selectOption", isCorrect)
+}
 </script>
 
 <template>
@@ -7,7 +15,12 @@ const {question} = defineProps(['question']);
         <h1 class="question">{{ question.text }}</h1>
     </div>
     <div class="options-container">
-        <div v-for="option in question.options" :key="option.id" class="option">
+        <div 
+            v-for="option in question.options" 
+            :key="option.id" 
+            class="option"
+            @click="emitSelectedOption(option.isCorrect)"
+        >
             <p class="option-label">{{ option.label }}</p>
             <div class="option-value">
                 <p>{{ option.text }}</p>
